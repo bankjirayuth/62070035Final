8f9a7780341eb3d86b6f683e0d5795d92162fb05
bankjirayuth <53044682+bankjirayuth@users.noreply.github.com>
2022-05-10 17:43:53 +0700

    upload prometheus and jenkinfile

---
+++ Jenkinsfile
@@ -0,0 +1,49 @@
+pipeline {
+    agent any
+    stages {
+        stage('Pull code') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Download front dependency') {
+            steps {
+                dir('front-end') {
+                    sh 'npm install'
+                }
+            }
+        }
+        stage('Download back dependency') {
+            steps {
+                dir('back-end') {
+                    sh 'npm install'
+                }
+            }
+        }
+        stage('Run unit test') {
+            steps {
+                echo 'pass'
+            }
+        }
+
+        stage('Run Component testing') {
+            steps {
+                echo 'pass'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                dir('front-end') {
+                    sh 'npm run build'
+                }
+            }
+        }
+
+        stage('Deployment') {
+            steps {
+                echo 'pass'
+            }
+        }
+    }
+}
--- back-end/index.js
+++ back-end/index.js
@@ -8,6 +8,27 @@ const port = 4000;
 app.use(cors());
 app.use(express.json());
 
+const { PrometheusExporter } = require("@opentelemetry/exporter-prometheus");
+const { MeterProvider } = require("@opentelemetry/sdk-metrics-base");
+
+// Add your port and startServer to the Prometheus options
+const options = { port: 9464, startServer: true };
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter("demo-prometheus");
+
+// Now, start recording data
+const counter = meter.createCounter("count_database", {
+  description: "count database",
+});
+
+
+
+
 mongoose.Promise = global.Promise;
 mongoose.connect(configs.mongouri, {
   useNewUrlParser: true,
@@ -15,10 +36,17 @@ mongoose.connect(configs.mongouri, {
 });
 
 app.post("/database", async (req, res) => {
+  counter.add(1, { service_name: "testservice" });
+  counter.add(1, { service_name: "testservice", status: "success", path: "/database" });
+  counter.add(1, { service_name: "testservice", status: "failure", path: "/database" });
+  counter.add(1, { service_name: "testservice", status: "data not found", path: "/database" });
+
   try {
     const result = await BlogModel.find();
-    const filtered = result.filter((data) => data.faculty_name == req.body.faculity);
-    console.log(filtered)
+    const filtered = result.filter(
+      (data) => data.faculty_name == req.body.faculity
+    );
+    console.log(filtered);
     res.send(filtered);
   } catch (err) {
     err;
